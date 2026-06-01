6d1c15f6ce003a0a18d9e02fce63797a7966e7f9
Chuse <joseluis.latas@gmail.com>
2026-06-01 23:20:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Roadmap from "./pages/Roadmap";
 import Identity from "./pages/Identity";
 import Card from "./pages/Card";
 import Wallet from "./pages/Wallet";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/identity" element={<Identity />} />
         <Route path="/card" element={<Card />} />
         <Route path="/wallet" element={<Wallet />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
