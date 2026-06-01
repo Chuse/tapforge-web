0f8338e9e9175fcae5ce95100d033b90fe6a34e0
Chuse <joseluis.latas@gmail.com>
2026-06-01 21:23:51 +0200

    Update App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Roadmap from "./pages/Roadmap";
+import Identity from "./pages/Identity";
 
 export default function App() {
   return (
